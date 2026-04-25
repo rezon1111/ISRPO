b14e30243151e85c503009702d9ed43474d5d758
rezon1111 <bodakyke@gmail.com>
2026-04-25 17:33:26 +0300

    Add files via upload

---
+++ Labs/LR15/SnakeGame/SnakeGame/Form1.Designer.cs
@@ -0,0 +1,28 @@
+﻿namespace SnakeGame
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+
+            // Form1
+            this.ClientSize = new System.Drawing.Size(500, 400);
+            this.Name = "Form1";
+            this.Text = "Snake Game";
+            this.BackColor = System.Drawing.Color.White;
+
+            this.ResumeLayout(false);
+        }
+    }
+}
